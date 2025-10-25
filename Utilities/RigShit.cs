b33eeb36fe9f868ef10f7d233ec9d84b9082a26c
Icy <jaycegamez98@gmail.com>
2025-10-25 16:57:23 -0700

    Add files via upload

---
+++ Utilities/RigShit.cs
@@ -0,0 +1,37 @@
+﻿using Photon.Pun;
+using UnityEngine;
+using Photon.Realtime;
+using HarmonyLib;
+using MalachiTemp.Backend;
+
+namespace dark.efijiPOIWikjek
+{
+    
+    internal class RigShit : MonoBehaviour
+    {
+        public static VRRig GetVRRigFromPlayer(Player p)
+        {
+            return GorillaGameManager.instance.FindPlayerVRRig(p);
+        }
+        public static Player GetPlayerFromVRRig(VRRig p)
+        {
+            return p.Creator.GetPlayerRef();
+        }
+        public static NetworkView GetNetworkFromRig(VRRig rig)
+        {
+            return (NetworkView)Traverse.Create(rig).Field("netView").GetValue();
+        }
+        public static Player GetPlayerFromGun()
+        {
+            return GetPlayerFromVRRig(Mods.raycastHit.collider.GetComponent<VRRig>());
+        }
+        public static PhotonView GetPhotonViewFromVRRig(VRRig p)
+        {
+            return (PhotonView)Traverse.Create(p).Field("photonView").GetValue();
+        }
+        public static VRRig GetOwnVRRig()
+        {
+            return GorillaTagger.Instance.offlineVRRig;
+        }
+    }
+}
